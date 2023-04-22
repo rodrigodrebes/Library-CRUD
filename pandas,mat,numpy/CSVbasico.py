51044e8e9f841920fc6451be7148ba6c12626305
Rodrigo Drebes <86028505+rodrigodrebes@users.noreply.github.com>
2023-04-22 15:17:17 -0300

    Delete pandas,mat,numpy directory

--- pandas,mat,numpy/CSVbasico.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Leitura de arquivo CSV
-pessoas = pd.read_csv('python\pandas,mat,numpy\myFile.csv')
-
-#selecionar colunas CSV
-coluna1 = pessoas['firstname']
-print(coluna1)
