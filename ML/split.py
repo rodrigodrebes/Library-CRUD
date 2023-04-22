8156170f90736307d70c4bba890031776fa84c34
Rodrigo Drebes <86028505+rodrigodrebes@users.noreply.github.com>
2023-04-22 15:17:00 -0300

    Delete ML directory

--- ML/split.py
+++
@@ -1,8 +0,0 @@
-class Empregado:
-          def __init__ (self, nome, sexo, salario):
-            self.nome = nome
-            self.sexo = sexo
-            self.salario = salario
-
-Fulano = Empregado("Fulano de Tal","m", 2500.00)
-print (Fulano.nome[0:6], Fulano.salario)
